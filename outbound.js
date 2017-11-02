fff8d0f2317a739a7a4c5bb0cdfcb38a97b9d16b
Alex Neigher <alex.neigher@snapdocs.com>
2017-11-02 12:47:29 -0600

    small list of random images

--- outbound.js
+++ outbound.js
@@ -17,7 +17,7 @@ $(function(){
 //generate some random content to prompt the user to log into the outbound
 function fetchNonLoggedInData(response){
   renderAdventureName('Want great content delivered daily to your new tabs?');
-  renderAdventureImage('https://www.theoutbound.com/assets/background_images/home-mtn-23f40cd56a9cc83dd2e1db77d04acf93f0403d7d7111bad02ac1824360c6780a.jpg');
+  renderAdventureImage(randomImageUrl());
   renderAdventureLink('/users/sign_in');
 }
 
@@ -43,7 +43,6 @@ function fetchLoggedInData(response){
 }
 
 
-
 function renderAdventureName(adventure_name){
   $("#adventure-name").html(adventure_name);
 }
@@ -54,4 +53,16 @@ function renderAdventureImage(adventure_image){
 
 function renderAdventureLink(adventure_link){
   $('#adventure-link').attr('href', "https://www.theoutbound.com/" + adventure_link);
+}
+
+function randomImageUrl(){
+  var images = [
+    "https://www.theoutbound.com/assets/background_images/home-mtn-23f40cd56a9cc83dd2e1db77d04acf93f0403d7d7111bad02ac1824360c6780a.jpg",
+    "https://images.theoutbound.com/uploads/1508264320434/c2gs3e69sc/2a06774847564f699ae464f5a4aa1d2a?w=1200&h=630&fit=crop",
+    "https://images.theoutbound.com/groups/170800/assets/1498446073209?w=900&fit=crop&auto=format&q=50&dpr=2"
+  ]
+
+  index = Math.floor(Math.random() * images.length);
+
+  return images[index];
 }
